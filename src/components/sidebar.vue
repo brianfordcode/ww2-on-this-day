6515919dd3f084fdd3cf8c24ff70d0191a7ed73a
Brian Ford <brianfordcode@gmail.com>
2022-02-02 02:00:13 -0800

    edit sideBar, add sub box

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -4,9 +4,19 @@
     class="sidebar-container"
     v-if="showSideBar"
 >
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
+    <div class="sub box">
+        <div class="sub-content">
+            <p>Sign up for the daily email:</p>
+            <input type="text" placeholder="Email">
+        </div>
+        
+    </div>
+    <div class="box">
+        adsense
+    </div>
+    <div class="box">
+        most popular days
+    </div>
 </div>
 
 
@@ -47,9 +57,32 @@ export default {
     box-shadow: 0px 0px 33px -20px #000000;
     background-color: rgb(206, 206, 206);
     height: 150px;
-    width: 200px;
+    width: 220px;
     margin-bottom: 20px;
     margin-left: 20px;
+}
+
+.sub {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    background-color: rgb(255, 255, 255);
+    text-align: center;
+}
+
+.sub-content p {
+    font-family: 'special elite';
+    width: 60%;
+    margin: 0 auto;
+}
+
+.sub-content input {
+    border: 1px solid black;
+    margin-top: 15px;
+    width: 70%;
+    padding: 5px;
+    outline: none;
 }
 
 </style>
