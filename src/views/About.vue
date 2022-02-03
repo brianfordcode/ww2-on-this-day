57937941ab9587b19b864470ac939cbb8f39c9aa
Brian Ford <brianfordcode@gmail.com>
2022-02-02 17:22:03 -0800

    start editing about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    <h1>about page</h1>
+  
+  <div class="about-container">
+
+    <h2 class="title">About World War 2 on this day</h2>
+
+    <div class="content">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/MacArthur_Manila.jpg/220px-MacArthur_Manila.jpg" alt="macarthur">
+      <p>
+        This site has been crafted from a love and respect of history and everything World War 2.
+        <br>
+        <br>
+        Follow along daily as each of the days events are highlighted.
+      </p>
+    </div>
+
   </div>
+
 </template>
+
+<style scoped>
+
+.about-container {
+  width: 100%;
+}
+
+.title {
+  text-align: center;
+  margin: 20px;
+}
+
+.content img {
+  float: left;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  
+}
+
+
+
+</style>
